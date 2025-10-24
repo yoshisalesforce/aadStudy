6a59caa74805630f9170b271d8c1f7055d01f848
Ryusei <yr.980827@gmail.com>
2025-10-24 17:07:27 +0900

    feat(classes,lwc,pages): update components and add unit tests

--- force-app/main/default/lwc/unitTest/unitTest.js
+++ force-app/main/default/lwc/unitTest/unitTest.js
@@ -1,8 +1,3 @@
-import { LightningElement, api } from 'lwc';
-import { sum } from './sum';
-export default class UnitTest extends LightningElement {
-  @api unitNumber = sum(2, 3);
-  handleChange(event) {
-    this.unitNumber = event.target.value;
-  }
-}
+import { LightningElement } from 'lwc';
+
+export default class UnitTest extends LightningElement {}
